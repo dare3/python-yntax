dcb1bef85da962cd3534dfd589fb2d13fd7e68b2
Dare3 <enitan_segun@yahoo.co.uk>
2024-06-06 12:54:19 -0400

    count up solved using range

--- count_up.py
+++ count_up.py
@@ -1,9 +1,20 @@
-def any7(nums):
-    """Are any of these numbers a 7? (True/False)"""
+def count_up(start, stop):
+    """Print all numbers from start up to and including stop.
+
+    For example:
+
+        count_up(5, 7)
+
+   should print:
+
+        5
+        6
+        7
+    """
+
     # YOUR CODE HERE
-    for num in nums:
-        if num == 7:
-            return True 
-    return False 
-print("should be true:-", any7([1, 2, 7, 4, 5]))
-print("should be false:-", any7([1, 2, 4, 5]))
+    for i in range(5,8):
+        print(i)
+
+
+count_up(5, 7)        
